837aa781adfad916837248c13876e0f767d245c1
Aashimajain18 <58872948+Aashimajain18@users.noreply.github.com>
2021-03-21 18:11:04 +0530

    Create Subarray Sums Divisible by K.cpp

---
+++ arrays/Subarray Sums Divisible by K.cpp
@@ -0,0 +1,34 @@
+/*
+Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+
+ 
+
+Example 1:
+
+Input: A = [4,5,0,-2,-3,1], K = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by K = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+ 
+
+Note:
+
+1 <= A.length <= 30000
+-10000 <= A[i] <= 10000
+2 <= K <= 10000
+*/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+        vector<int> count(K);
+        count[0] = 1;
+        int prefix = 0, res = 0;
+        for (int a : A) {
+            prefix = (prefix + a % K + K) % K;
+            res += count[prefix]++;
+        }
+        return res;
+
+    }
+};
