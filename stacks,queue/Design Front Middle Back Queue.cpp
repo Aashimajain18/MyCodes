9a9cb96985c3266639fd1065e63ec2832f520321
Aashimajain18 <58872948+Aashimajain18@users.noreply.github.com>
2021-04-25 23:02:39 +0530

    Create Design Front Middle Back Queue.cpp

---
+++ stacks,queue/Design Front Middle Back Queue.cpp
@@ -0,0 +1,108 @@
+/*
+Design a queue that supports push and pop operations in the front, middle, and back.
+
+Implement the FrontMiddleBack class:
+
+FrontMiddleBack() Initializes the queue.
+void pushFront(int val) Adds val to the front of the queue.
+void pushMiddle(int val) Adds val to the middle of the queue.
+void pushBack(int val) Adds val to the back of the queue.
+int popFront() Removes the front element of the queue and returns it. If the queue is empty, return -1.
+int popMiddle() Removes the middle element of the queue and returns it. If the queue is empty, return -1.
+int popBack() Removes the back element of the queue and returns it. If the queue is empty, return -1.
+Notice that when there are two middle position choices, the operation is performed on the frontmost middle position choice. For example:
+
+Pushing 6 into the middle of [1, 2, 3, 4, 5] results in [1, 2, 6, 3, 4, 5].
+Popping the middle from [1, 2, 3, 4, 5, 6] returns 3 and results in [1, 2, 4, 5, 6].
+ 
+
+Example 1:
+
+Input:
+["FrontMiddleBackQueue", "pushFront", "pushBack", "pushMiddle", "pushMiddle", "popFront", "popMiddle", "popMiddle", "popBack", "popFront"]
+[[], [1], [2], [3], [4], [], [], [], [], []]
+Output:
+[null, null, null, null, null, 1, 3, 4, 2, -1]
+
+Explanation:
+FrontMiddleBackQueue q = new FrontMiddleBackQueue();
+q.pushFront(1);   // [1]
+q.pushBack(2);    // [1, 2]
+q.pushMiddle(3);  // [1, 3, 2]
+q.pushMiddle(4);  // [1, 4, 3, 2]
+q.popFront();     // return 1 -> [4, 3, 2]
+q.popMiddle();    // return 3 -> [4, 2]
+q.popMiddle();    // return 4 -> [2]
+q.popBack();      // return 2 -> []
+q.popFront();     // return -1 -> [] (The queue is empty)
+ 
+
+Constraints:
+
+1 <= val <= 10^9
+At most 1000 calls will be made to pushFront, pushMiddle, pushBack, popFront, popMiddle, and popBack.
+*/
+
+
+class FrontMiddleBackQueue {
+
+    deque<int> d;
+
+public:
+    
+    FrontMiddleBackQueue() {
+        d.clear();
+    }
+    
+    void pushFront(int val) {
+        d.push_front(val);
+    }
+    
+    void pushMiddle(int val) {
+          int n = d.size();
+        d.insert(d.begin() + n / 2, val);
+    }
+    
+    void pushBack(int val) {
+        d.push_back(val);
+    }
+    
+    int popFront() {
+        if(!d.empty()){
+            int ans=d.front();
+         d.pop_front();
+            return ans;
+        }
+        return -1;
+    }
+    
+    int popMiddle() {
+               if(d.empty()) return -1;
+        
+        int mid = (d.size() - 1) / 2;
+        int ans = *(d.begin() + mid);
+        d.erase(d.begin() + mid);
+        return ans;
+
+    }
+    
+    int popBack() {
+       if(!d.empty()){
+         int ans=d.back();
+         d.pop_back();
+            return ans;
+        }
+        return -1; 
+    }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
