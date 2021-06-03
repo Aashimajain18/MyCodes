32b90526ed34a03134a5294e060b1ac3236279f5
Aashimajain18 <58872948+Aashimajain18@users.noreply.github.com>
2021-06-03 23:55:32 +0530

    Create Maximum Units on a Truck.cpp

---
+++ greedy/Maximum Units on a Truck.cpp
@@ -0,0 +1,71 @@
+/*
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+ 
+
+Example 1:
+
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+Example 2:
+
+Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+Output: 91
+ 
+
+Constraints:
+
+1 <= boxTypes.length <= 1000
+1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+1 <= truckSize <= 10^6
+*/
+
+//my solution
+//imp to see how to sort vector of vector according to column or row
+class Solution {
+public:
+static bool sortcol( const vector<int>& v1,
+               const vector<int>& v2 ) {
+ return v1[1] > v2[1];
+}
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(),sortcol);
+        int n=boxTypes.size();
+        int val=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(val==truckSize){
+                break;
+            }
+            if(val+boxTypes[i][0]<truckSize){
+                val+=boxTypes[i][0];
+                ans+=boxTypes[i][0]*boxTypes[i][1];
+            }
+            else{
+             for(int j=1;j<=boxTypes[i][0];j++){
+                 if(val+1>truckSize){
+                     break;
+                 }
+                 else{
+                     val+=1;
+                     ans+=boxTypes[i][1];
+                 }
+             }   
+            }
+        }
+
+        return ans;
+    }
+};
